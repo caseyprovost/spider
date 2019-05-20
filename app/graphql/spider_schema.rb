34ad089989e3bb4741ef8d09ae53327d6e3b0af5
Casey Provost <casey.provost@spreemo.com>
2019-05-20 08:12:28 -0400

    initial commit

---
+++ app/graphql/spider_schema.rb
@@ -0,0 +1,4 @@
+class SpiderSchema < GraphQL::Schema
+  mutation(Types::MutationType)
+  query(Types::QueryType)
+end
---
+++ app/graphql/types/book_type.rb
@@ -0,0 +1,18 @@
+module Types
+  class BookType < Types::BaseObject
+    field :title, String, null: false
+    field :summary, String, null: true
+    field :page_count, Integer, null: true
+    field :publication_date, Types::ISO8601DateTimeType, null: true
+    field :publisher, Types::PublisherType, null: true
+    field :author, Types::AuthorType, null: true
+  end
+
+  class AuthorType < Types::BaseObject
+    field :name, String, null: false
+  end
+
+  class PublisherType < Types::BaseObject
+    field :name, String, null: false
+  end
+end
