e7e4f82a69bd7ff63702377b12f2010c3f910fe1
Casey Provost <casey.provost@spreemo.com>
2019-07-04 13:17:38 -0400

    debugging

--- app/graphql/types/product_type.rb
+++ app/graphql/types/product_type.rb
@@ -1,7 +1,16 @@
 module Types
   class ProductType < Types::BaseObject
+    field :id, ID, null: false
     field :name, String, null: false
     field :description, String, null: true
     field :variants, [Types::VariantType], null: true
+
+    def self.scope_items(items, context)
+      Rails.logger.info(%Q{
+        CONTEXT IS \n
+        #{context.inspect} \n
+      }
+      items.where(product_id: 1)
+    end
   end
 end
--- app/graphql/types/variant_type.rb
+++ app/graphql/types/variant_type.rb
@@ -1,5 +1,6 @@
 module Types
   class VariantType < Types::BaseObject
+    field :id, ID, null: false
     field :name, String, null: false
     field :price, String, null: true
     field :sku, Integer, null: true
