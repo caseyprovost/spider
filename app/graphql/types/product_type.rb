00092a08a003428010b3cc5d75b8e36080d55358
Casey Provost <casey.provost@spreemo.com>
2019-07-04 13:04:01 -0400

    fix product type

--- app/graphql/types/product_type.rb
+++ app/graphql/types/product_type.rb
@@ -2,6 +2,6 @@ module Types
   class ProductType < Types::BaseObject
     field :name, String, null: false
     field :description, String, null: true
-    field :variants, [Types::AuthorType], null: true
+    field :variants, [Types::VariantType], null: true
   end
 end
