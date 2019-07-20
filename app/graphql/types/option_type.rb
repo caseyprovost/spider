2cf6c70485bc97cfaae3a5eb061ff727b5d36d84
Casey Provost <caseyr.provost@gmail.com>
2019-07-20 12:41:42 -0400

    clean up option types

--- app/graphql/types/option_type.rb
+++ app/graphql/types/option_type.rb
@@ -1,17 +1,5 @@
 module Types
   class OptionType < Types::BaseObject
     field :name, String, null: true
-    field :option_types, [Types::OptionType], null: true
-
-    def product_option_types
-      filters = {option_type_id: object["id"]}
-      api_client(context[:jwt_token]).option_types(filters: filters)
-    end
-
-    private
-
-    def api_client(jwt_token)
-      ApiClient.new(jwt_token)
-    end
   end
 end
