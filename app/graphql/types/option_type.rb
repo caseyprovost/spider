1c99f2a915f66fc6023aa8330bcdf102e5483e46
Casey Provost <caseyr.provost@gmail.com>
2019-07-20 12:32:44 -0400

    add product option types to option types

--- app/graphql/types/option_type.rb
+++ app/graphql/types/option_type.rb
@@ -1,5 +1,17 @@
 module Types
   class OptionType < Types::BaseObject
     field :name, String, null: true
+    field :option_types, [Types::OptionType], null: true
+
+    def product_option_types
+      filters = {option_type_id: object["id"]}
+      api_client(context[:jwt_token]).option_types(filters: filters)
+    end
+
+    private
+
+    def api_client(jwt_token)
+      ApiClient.new(jwt_token)
+    end
   end
 end
