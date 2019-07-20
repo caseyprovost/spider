2ac9960a54f00f38c7ed9543c193f149fac3454a
Casey Provost <caseyr.provost@gmail.com>
2019-07-20 14:03:47 -0400

    try camel case for relationship IDS

--- app/graphql/types/product_property_type.rb
+++ app/graphql/types/product_property_type.rb
@@ -2,17 +2,15 @@ module Types
   class ProductPropertyType < Types::BaseObject
     field :id, ID, null: false
     field :value, String, null: false
-    field :property_id, String, null: false,
-    field :product_id, String, null: false
     field :property, Types::PropertyType, null: false
     field :product, Types::ProductType, null: false
 
     def property
-      api_client(context[:jwt_token]).property(object["property_id"])
+      api_client(context[:jwt_token]).property(object["propertyId"])
     end
 
     def product
-      api_client(context[:jwt_token]).product(object["product_id"])
+      api_client(context[:jwt_token]).product(object["productId"])
     end
 
     private
