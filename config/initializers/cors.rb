438043bf87ac25d239869f50581b6f3e551b34b5
Casey Provost <caseyr.provost@gmail.com>
2019-07-27 14:01:44 -0400

    enable building against localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost'
+    origins [
+    	'localhost:3000',
+    	'localhost:3001'
+    ]
 
     resource '*',
       headers: :any,
