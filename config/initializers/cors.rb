d576c37705001446e385151acbc38e06031660c7
Casey Provost <caseyr.provost@gmail.com>
2019-07-27 14:14:43 -0400

    more cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,9 +10,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: :any
   end
 end
